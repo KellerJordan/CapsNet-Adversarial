implement Adversary class, conduct experiments on CapsNet -- it is surprisingly robust, probably due to gradient masking effects, fix bugs in capsnet

adversarial.py
@@ -7,31 +7,57 @@ def set_grad(model, cond):
     for p in model.parameters():
         p.requires_grad = cond
 
-def generate_fooling_image(model, seed_img, target=5,
-                  n_iters=100, alpha=0.005):
-    """
-    inputs: seed_img (Tensor), model
-    outputs: fool_img (Tensor)
-    """
-    set_grad(model, False)
+class Adversary():
     
-    img_var = Variable(seed_img.clone(), requires_grad=True)
+    def __init__(self, model):
+        self.model = model
     
-    for _ in range(n_iters):
+    def generate_example(self, seed_img, target_class, attack, **kwargs):
+        set_grad(self.model, False)
+        img_var = Variable(seed_img.clone(), requires_grad=True)
         
-        scores = model(img_var)
-        objective = scores.squeeze()[target]
-        objective.backward()
+        if attack == 'GA':
+            fool_img = self.GA(img_var, target_class, **kwargs)
+        elif attack == 'FGS':
+            fool_img = self.FGS(img_var, target_class, **kwargs)
+        else:
+            raise Exception('[!] Unknown attack method specified')
         
-        g = img_var.grad.data.clone()
-        img_var.grad.zero_()
-        g = g / g.norm()
+        set_grad(self.model, True)
+        return fool_img
+    
+    
+    
+    # gradient ascent
+    def GA(self, img_var, target, n_iters=100, eta=0.005):
+        for _ in range(n_iters):
+            scores = self.model(img_var)
+            objective = scores.squeeze()[target]
+            objective.backward()
+
+            g = img_var.grad.data.clone()
+            img_var.grad.zero_()
+            g = g / g.norm()
+
+            step = eta * g
+            img_var.data += step.cuda()
+            img_var.data = torch.clamp(img_var.data, min=-1, max=1)
         
-        step = alpha * g
-        img_var.data += step.cuda()
-        img_var.data = torch.clamp(img_var.data, min=-1, max=1)
+        return img_var.data
+
+    # fast gradient sign
+    def FGS(self, img_var, target, n_iters=100, eta=0.005):
+        for _ in range(n_iters):
+            scores = self.model(img_var)
+            objective = scores.squeeze()[target]
+            objective.backward()
+            
+            g = img_var.grad.data.clone()
+            g = g.abs() / (g + 1e-4) # sign of gradient
+            
+            step = eta * g
+            img_var.data += step.cuda()
+            img_var.data = torch.clamp(img_var.data, min=-1, max=1)
+        
+        return img_var.data
     
-    fool_img = img_var.data.clone()
-    
-    set_grad(model, True)
-    return fool_img

capsnet.py
@@ -53,7 +53,7 @@ class CapsuleLayer(nn.Module):
             outputs = self.squash(x.transpose(1, 2))
             # batch_size, out_caps, out_dim
         else:
-            u_hats = (x[:, None, :, None, :] @ self.route_weights[...]).squeeze()
+            u_hats = (x[:, None, :, None, :] @ self.route_weights[...]).squeeze(3)
             u_hats = u_hats.transpose(2, 3)
             # batch_size, out_caps, out_dim, in_caps
             ## batch_size, out_caps, in_caps, out_dim
@@ -66,14 +66,15 @@ class CapsuleLayer(nn.Module):
                 probs = F.softmax(logits, dim=1)
                 # batch_size, in_caps, out_caps
 
-                capsule_s = (u_hats @ probs.transpose(1, 2)[..., None]).squeeze()
+                capsule_s = (u_hats @ probs.transpose(1, 2)[..., None]).squeeze(3)
                 outputs = self.squash(capsule_s)
                 # batch_size, out_caps, out_dim
                 
-                updates = (outputs[..., None, :] @ u_hats).squeeze().transpose(1, 2)
-                # batch_size, in_caps, out_caps
-                
-                logits = logits + updates
+                if i < self.routing_iters - 1:
+                    updates = (outputs[..., None, :] @ u_hats).squeeze(2).transpose(1, 2)
+                    # batch_size, in_caps, out_caps
+
+                    logits = logits + updates
         
         return outputs
 
@@ -95,32 +96,53 @@ class ReconstructionNetwork(nn.Module):
 
 class CapsuleDecoder(nn.Module):
     
-    def __init__(self, reconstruction=True):
+    def __init__(self, reconstruction=True, mask_incorrect=True):
         super().__init__()
         self.reconstruction = reconstruction
         if reconstruction:
             self.decoder = ReconstructionNetwork()
+            self.mask_incorrect = mask_incorrect
     
-    def forward(self, d_caps):
+    def forward(self, d_caps, labels=None):
         logits = torch.norm(d_caps, dim=-1)
-        probs = F.softmax(logits, dim=1) # THIS SHOULD NOT NECESSARILY BE HERE
+        probs = F.softmax(logits, dim=1) # only for single-digit classification task
         if self.reconstruction:
-            img_hats = self.decoder(d_caps.view(d_caps.size(0), -1))
+            if self.mask_incorrect:
+                # mask all but the correct (maximum for unsupervised) digit
+                if labels is None:
+                    _, class_indices = logits.max(1)
+                    identity = Variable(torch.sparse.torch.eye(10)).type(d_caps.data.type())
+                    y = identity.index_select(0, class_indices)
+                else:
+                    y = labels
+                masked_caps = d_caps * y[..., None]
+                decoder_input = masked_caps.view(masked_caps.size(0), -1)
+            else:
+                # or don't -- for some reason this leads to mode collapse!
+                decoder_input = d_caps.view(d_caps.size(0), -1)
+            
+            img_hats = self.decoder(decoder_input)
             return probs, img_hats
         else:
             return probs
 
 class CapsuleLoss(nn.Module):
     
-    def forward(self, probs, labels, reconstructions=None, images=None,
-                mplus=0.9, mminus=0.1, lmbda=0.5, tradeoff=0.0005):
-        present = F.relu(mplus - probs)**2
-        absent = F.relu(probs - mminus)**2
-        margin_loss = labels * present + lmbda * (1 - labels) * absent
-        margin_loss = margin_loss.sum(1)
+    def __init__(self, mplus=0.9, mminus=0.1, lmbda=0.5, tradeoff=0.0005):
+        super().__init__()
+        self.mplus = mplus
+        self.mminus = mminus
+        self.lmbda = lmbda
+        self.tradeoff = tradeoff
+    
+    def forward(self, probs, labels, reconstructions=None, images=None):
+        present = F.relu(self.mplus - probs)**2
+        absent = F.relu(probs - self.mminus)**2
+        margin_loss = labels * present + self.lmbda * (1 - labels) * absent
+        margin_loss = margin_loss.sum(1).sum(0)
         if reconstructions is not None:
-            reconstruction_loss = F.mse_loss(reconstructions, images)
-            loss = margin_loss + tradeoff * reconstruction_loss
+            reconstruction_loss = F.mse_loss(reconstructions, images, size_average=False)
+            loss = margin_loss + self.tradeoff * reconstruction_loss
         else:
             loss = margin_loss
         return loss.mean()

utils.py
@@ -9,7 +9,7 @@ from torch.autograd import Variable
 import torchvision
 
 
-def plot_tensor(img, fs=(10, 10), title=''):
+def plot_tensor(img, fs=(8, 8), title=''):
     # preprocess input
     if type(img) == Variable:
         img = img.data
@@ -19,9 +19,9 @@ def plot_tensor(img, fs=(10, 10), title=''):
     else:
         npimg = img
     if len(npimg.shape) == 4:
-        npimg = np.squeeze(npimg)
+        npimg = npimg[0]
+#         npimg = np.squeeze(npimg)
     npimg = npimg.transpose(1, 2, 0)
-    # display images
     plt.figure(figsize=fs)
     if npimg.shape[2] > 1:
         plt.imshow(npimg)
@@ -113,7 +113,7 @@ class Trainer():
             y_var = self.make_var(one_hotify(y) if self.one_hot else y)
 
             if self.use_reconstructions:
-                scores, reconstructions = self.model(X_var)
+                scores, reconstructions = self.model(X_var, y_var)
                 loss_var = self.criterion(scores, y_var, reconstructions, X_var)
             else:
                 scores = self.model(X_var)